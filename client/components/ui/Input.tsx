f14925c41b758b91ab312c4a80aab4894e48ccd7
Rishi Mehto <mehtorishi62203@gmail.com>
2026-03-06 20:43:02 +0530

    Added the track order page

--- client/components/ui/Input.tsx
+++ client/components/ui/Input.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes, forwardRef } from 'react'
+import { InputHTMLAttributes, forwardRef, useId } from 'react'
 import { cn } from '@/lib/utils'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -9,7 +9,8 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ className, label, error, helperText, type = 'text', id, ...props }, ref) => {
-    const inputId = id || `input-${Math.random().toString(36).substr(2, 9)}`
+    const generatedId = useId()
+    const inputId = id || `input-${generatedId.replace(/:/g, '')}`
     
     return (
       <div className="w-full">
